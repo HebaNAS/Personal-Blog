7d50e69cb4dd9b846e821833b4d6bfbc1b8d71a5
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-07 14:25:30 +0400

    Fixing deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem', 
       options: { 
         name: `pages`,
-        path: `${__dirname}/src/pages`,
+        path: `src/pages`,
         ignore: `[**/.*]`,
       },
     },
