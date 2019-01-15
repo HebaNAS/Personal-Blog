8539f9d14e804e124b8d5060d158853fe237dfba
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-15 15:14:50 +0400

    Fixing bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import AOS from 'aos'
+//import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Layout from '../components/layout'
 import Animation from '../components/Animation'
 import About from '../components/About'
 
-AOS.init()
+//AOS.init()
 
-const IndexPage = (props) => (
-  <Layout location={props.location}>
-    <Animation />
-    <About />
-    <Link to="/blog/">Go to Blog Page</Link>
-  </Layout>
-)
+class IndexPage extends Component{
+  constructor(props) {
+    super(props)
+  }
+  
+  componentDidMount() {
+    const isBrowser = typeof window !== 'undefined'
+    const AOS = isBrowser ? require('aos') : 'undefined'
 
+    this.aos = AOS
+    this.aos.init()
+  }
+
+  componentDidUpdate() {
+    this.aos.refresh()
+  }
+  
+  render() {
+    return(
+      <Layout location={props.location}>
+        <Animation />
+        <About />
+        <Link to="/blog/">Go to Blog Page</Link>
+      </Layout>
+    )
+  }
+}
 export default IndexPage
