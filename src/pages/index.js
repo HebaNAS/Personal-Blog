783b6346b0254e60148018f476f9aaec8bf2e0c2
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-15 16:14:35 +0400

    Tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'gatsby'
-//import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Layout from '../components/layout'
 import Animation from '../components/Animation'
 import About from '../components/About'
 
-//AOS.init()
-
 class IndexPage extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   props = this.props
-  // }
-  
   componentDidMount() {
     const isBrowser = typeof window !== 'undefined'
     const AOS = isBrowser ? require('aos') : 'undefined'
@@ -31,7 +22,6 @@ class IndexPage extends Component {
       <Layout location={this.props.location}>
         <Animation />
         <About />
-        <Link to="/blog/">Go to Blog Page</Link>
       </Layout>
     )
   }
