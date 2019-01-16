7e0eb5a2b9fdd3c64ae340541a6edd71b5b5168e
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-16 12:33:55 +0400

    Small edits

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,22 +24,25 @@ class Header extends Component {
       })
     }
 
-    // Hide menu on scroll
-    let previousScroll = 0
 
-    $(window).scroll(function(){
-      let currentScroll = $(this).scrollTop()
+    if ($(window).width() > 768) {
+      // Hide menu on scroll
+      let previousScroll = 0
 
-      if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
-        if (currentScroll > previousScroll) {
-          window.setTimeout(hideNav, 300)
-        } else {
-          window.setTimeout(showNav, 300)
+      $(window).scroll(function(){
+        let currentScroll = $(this).scrollTop()
+
+        if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+          if (currentScroll > previousScroll) {
+            window.setTimeout(hideNav, 300)
+          } else {
+            window.setTimeout(showNav, 300)
+          }
+
+          previousScroll = currentScroll
         }
-
-        previousScroll = currentScroll
-      }
-    })
+      })
+    }
 
     function hideNav() {
       $("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden")
