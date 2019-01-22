33645f29653bd434e4b9ab8a2ad814a5c58151ce
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-22 16:51:55 +0400

    Fixing css after redirects

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 import $ from 'jquery'
 
-import Layout from '../components/layout'
+import typography from '../utils/typography'
 
+import '../scss/main.scss'
 import '../scss/error.scss'
 
 class NotFoundPage extends Component {
@@ -27,44 +29,55 @@ class NotFoundPage extends Component {
 
   render() {
     return(
-      <Layout>
-        <div className="box">
-          <div className="box__ghost">
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            
-            <div className="box__ghost-container">
-              <div className="box__ghost-eyes">
-                <div className="box__eye-left"></div>
-                <div className="box__eye-right"></div>
+      <div>
+        <Helmet
+            TypographyStyle={[ {typography: typography} ]}
+            GoogleFont={[ {typography: typography} ]}
+          />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 1100,
+            }}
+          >
+            <div className="box">
+              <div className="box__ghost">
+                <div className="symbol"></div>
+                <div className="symbol"></div>
+                <div className="symbol"></div>
+                <div className="symbol"></div>
+                <div className="symbol"></div>
+                <div className="symbol"></div>
+                
+                <div className="box__ghost-container">
+                  <div className="box__ghost-eyes">
+                    <div className="box__eye-left"></div>
+                    <div className="box__eye-right"></div>
+                  </div>
+                  <div className="box__ghost-bottom">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
+                <div className="box__ghost-shadow"></div>
               </div>
-              <div className="box__ghost-bottom">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+              
+              <div className="box__description">
+                <div className="box__description-container">
+                  <div className="box__description-title">Whoops!</div>
+                  <div className="box__description-text">It seems like we couldn't find the page you were looking for</div>
+                </div>
+                
+                <a href="/" className="box__button">Go back</a>
+                
               </div>
+              
             </div>
-            <div className="box__ghost-shadow"></div>
           </div>
-          
-          <div className="box__description">
-            <div className="box__description-container">
-              <div className="box__description-title">Whoops!</div>
-              <div className="box__description-text">It seems like we couldn't find the page you were looking for</div>
-            </div>
-            
-            <a href="/" className="box__button">Go back</a>
-            
-          </div>
-          
         </div>
-      </Layout>
     )
   }
 }
--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 import { navigate } from '@reach/router'
 
-import Layout from '../components/layout'
+import typography from '../utils/typography'
 
+import '../scss/main.scss'
 import '../scss/thankyou.scss'
 
 class ThankYou extends Component {
@@ -32,29 +34,38 @@ class ThankYou extends Component {
   }
 
   render() {
+    this.timer(10000)
     return(
-      <Layout location={this.props.location}>
-        <div>
-          {this.timer(10000)}
-          <div id="mail-container">
-            <div id="line-container">
-                <div className="line line-1"></div>
-                <div className="line line-4"></div>
-                <div className="line line-2"></div>
-                <div className="line line-5"></div>
-                <div className="line line-3"></div>
+      <div>
+        <Helmet
+            TypographyStyle={[ {typography: typography} ]}
+            GoogleFont={[ {typography: typography} ]}
+          />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 1100,
+            }}
+          >
+            <div id="mail-container">
+              <div id="line-container">
+                  <div className="line line-1"></div>
+                  <div className="line line-4"></div>
+                  <div className="line line-2"></div>
+                  <div className="line line-5"></div>
+                  <div className="line line-3"></div>
+              </div>
+              <div id="mail"></div>
             </div>
-            <div id="mail"></div>
-          </div>
-          <p className="thanks-message is-size-2 has-text-centered is-fullwidth">Your message has been sent!</p>
-          <div className="box">
-            <div className="box__description">
-              <a href="/" className="box__button">Go back</a>
+            <p className="thanks-message is-size-2">Your message has been sent!</p>
+            <div className="box">
+              <div className="box__description">
+                <a href="/" className="box__button">Go back</a>
+              </div>
             </div>
+            <p className="redirect is-size-6">You will be redirected in <span className="timer" ref={this.counter}>{this.t}</span> seconds.</p>
           </div>
-          <p className="redirect is-size-6 has-text-centered is-fullwidth">You will be redirected in <span className="timer" ref={this.counter}>{this.t}</span> seconds.</p>
-        </div>
-      </Layout>
+      </div>
     )
   }
 }
