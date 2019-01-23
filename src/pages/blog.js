c4dda5d3c0ad632721edf8c8e376a0d83f42f603
Heba El-Shimy <HebaNAS@users.noreply.github.com>
2019-01-23 13:17:25 +0400

    Fixing meta tags

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 import '../scss/blog.scss'
 
 const Blog = (props) => {
-  const post = props.data.markdownRemark
-  const { title } = post.frontmatter
+  // const post = props.data.markdownRemark
+  // const { title } = post.frontmatter
 
   return(
   <Layout>
@@ -27,13 +27,13 @@ const Blog = (props) => {
 
 export default Blog
 
-const query = graphql`
- query PostQuery($slug: String!) {
-     markdownRemark(fields: { slug: { eq: $slug } }) {
-       html
-       frontmatter {
-        title
-        description
-       }
-   }
-`
+// const query = graphql`
+//  query PostQuery($slug: String!) {
+//      markdownRemark(fields: { slug: { eq: $slug } }) {
+//        html
+//        frontmatter {
+//         title
+//         description
+//        }
+//    }
+// `
